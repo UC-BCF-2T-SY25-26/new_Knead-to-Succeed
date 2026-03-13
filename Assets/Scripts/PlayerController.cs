36344b8f0edba187729f390560465dd261e468d8
ochoco-hub <abo1923@students.uc-bcf.edu.ph>
2026-03-13 21:21:52 +0800

    Instructions completed, triggers undone

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,40 +1,21 @@
 using UnityEngine;
 
-public class SimpleMovement : MonoBehaviour
+public class SimplePlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float speed = 5f;
 
     void Update()
     {
-        float x = 0f;
-        float z = 0f;
-
         if (Input.GetKey(KeyCode.W))
-            z += 1f;
+            transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
 
         if (Input.GetKey(KeyCode.S))
-            z -= 1f;
+            transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
 
         if (Input.GetKey(KeyCode.A))
-            x -= 1f;
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
 
         if (Input.GetKey(KeyCode.D))
-            x += 1f;
-
-        // Calculate movement vector
-        Vector3 move = new Vector3(x, 0f, z);
-
-        // If there's any movement, rotate the player to face the direction
-        if (move.magnitude > 0f)
-        {
-            // Normalize the move vector to avoid diagonal movement being faster
-            move.Normalize();
-
-            // Rotate the character to face the movement direction
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), Time.deltaTime * 10f);
-        }
-
-        // Move the player
-        transform.position += move * moveSpeed * Time.deltaTime;
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
     }
 }
